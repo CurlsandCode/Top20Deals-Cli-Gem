8c54102794c4e1a18108b1c5ae511ea85dced9b0
Monika <monika.m.williams@gmail.com>
2017-05-21 21:31:35 +0900

    created menu method

--- lib/Top_20_Deals/cli.rb
+++ lib/Top_20_Deals/cli.rb
@@ -15,6 +15,21 @@ class Top20Deals::CLI
     
     def menu
         input = nil
+        while input != "exit"
+        "Puts enter the number (1-20) of the deal you would like more information on. Or type 'list' to see the list again or 'exit'."
+            input = gets.strip.downcase
+            if input.to_i > 0 && input.to_i <= 20
+            puts "1.Upscale Mexico All-Inclusive Retreat incl. flights - Riviera Nayarit - $529 - This luxe retreat offers a new spin on the traditional all-inclusive experience (and we love it)."
+            puts "2.Upscale France All-Inclusive Retreat incl. flights - Nice - $829 -  This luxe retreat offers a new spin on the traditional all-inclusive experience (and we love it)."
+            puts "3.Upscale Jamaica All-Inclusive Retreat incl. flights - Kingston - $629 -  This luxe retreat offers a new spin on the traditional all-inclusive experience (and we love it)." 
+                elsif input = "list"
+                list_deals
+                elsif input = "exit"
+                goodbye
+                else
+                "Opps try again, invalid entry!"
+           end
+        end
     end
     
     def goodbye
