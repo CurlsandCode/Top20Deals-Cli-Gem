ec7c16289d1625b6a58c76d12b4fad1db6d37e7a
Monika <monika.m.williams@gmail.com>
2017-05-21 21:40:55 +0900

    fixed menu so that the output matched what was expected. finish goodbye method.

--- lib/Top_20_Deals/cli.rb
+++ lib/Top_20_Deals/cli.rb
@@ -16,15 +16,17 @@ class Top20Deals::CLI
     def menu
         input = nil
         while input != "exit"
-        "Puts enter the number (1-20) of the deal you would like more information on. Or type 'list' to see the list again or 'exit'."
+         puts "Enter the number (1-20) of the deal you would like more information on. Or type 'list' to see the list again or 'exit'."
             input = gets.strip.downcase
-            if input.to_i > 0 && input.to_i <= 20
+            if input == "1"
             puts "1.Upscale Mexico All-Inclusive Retreat incl. flights - Riviera Nayarit - $529 - This luxe retreat offers a new spin on the traditional all-inclusive experience (and we love it)."
+            elsif input == "2"
             puts "2.Upscale France All-Inclusive Retreat incl. flights - Nice - $829 -  This luxe retreat offers a new spin on the traditional all-inclusive experience (and we love it)."
+             elsif input == "3"
             puts "3.Upscale Jamaica All-Inclusive Retreat incl. flights - Kingston - $629 -  This luxe retreat offers a new spin on the traditional all-inclusive experience (and we love it)." 
-                elsif input = "list"
+                elsif input == "list"
                 list_deals
-                elsif input = "exit"
+                elsif input == "exit"
                 goodbye
                 else
                 "Opps try again, invalid entry!"
@@ -33,5 +35,6 @@ class Top20Deals::CLI
     end
     
     def goodbye
+        puts "See you next time!"
     end
 end
