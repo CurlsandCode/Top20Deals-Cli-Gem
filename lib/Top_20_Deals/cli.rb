dd09c5bbb48d25e353f2f031919eada641f0b928
Monika <monika.m.williams@gmail.com>
2017-05-21 21:11:24 +0900

    created cli class and added some methods

---
+++ lib/Top_20_Deals/cli.rb
@@ -0,0 +1,18 @@
+class Top20Deals::CLI
+    
+    def call
+        list_deals
+        menu
+        goodbye
+    end
+    
+    def list_deals
+         puts "hello world"
+    end
+    
+    def menu
+    end
+    
+    def goodbye
+    end
+end
