2a4d03cd878fbaa1a414b9f30f2cfc42fc51bedf
Monika <monika.m.williams@gmail.com>
2017-05-21 21:22:32 +0900

    added fake data to test list_deals method

--- lib/Top_20_Deals/cli.rb
+++ lib/Top_20_Deals/cli.rb
@@ -7,10 +7,14 @@ class Top20Deals::CLI
     end
     
     def list_deals
-         puts "hello world"
-    end
+         puts "Hi! Here are this weeks Top 20 Travel Deals:"
+         puts "1.Upscale Mexico All-Inclusive Retreat incl. flights - Riviera Nayarit - $529"
+         puts "2.Upscale France All-Inclusive Retreat incl. flights - Nice - $829"
+         puts "3.Upscale Jamaica All-Inclusive Retreat incl. flights - Kingston - $629"
+        end
     
     def menu
+        input = nil
     end
     
     def goodbye
