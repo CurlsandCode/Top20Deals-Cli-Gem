07f2bbc09337574f9921699d6b858c7bebd87e65
Monika <monika.m.williams@gmail.com>
2017-05-21 22:04:31 +0900

    added #scrape_travelzoo_index method and #make_deals method to scraper class.

--- lib/Top_20_Deals/scraper.rb
+++ lib/Top_20_Deals/scraper.rb
@@ -1,6 +1,15 @@
 Class Top20Deals::Deals
 
- def get_page
+    def get_page
      Nokogiri::HTML(open("https://www.travelzoo.com.top20/"))
- end
+    end
+
+     def scrape_travelzoo_index
+      self.get_pages.css('div.deal-info.clearfix')
+     end
+
+    def makes_deals
+      scrape_travelzoo_index.each do |deal|
+      Top20Deals::Deals.new_from_index_page(deal)
+      end
 end
